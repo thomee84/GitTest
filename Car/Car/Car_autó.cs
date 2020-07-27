b43794f527806e4500b8e85de0dfa0f37dae5a5f
Tamas Csendes <tacsendes@gmail.com>
2020-07-27 19:18:40 +0200

    friss

--- Car/Car/Car_autó.cs
+++ Car/Car/Car_autó.cs
@@ -8,30 +8,7 @@ namespace Car
     class Car_autó
     {
 
-        //private string name; //field
-        public string Name //property
-        
-            { get; set; }
-        
+        public string color = "red";
 
-
-        //2
-        /*public string model;
-        public string type;
-        public string year;
-        public Car_autó(string modelName, string modelType, string modelYear)
-        {
-            model = modelName;
-            type = modelType;
-            year = modelYear;
-        }*/
-        /*// Class members
-        public string color = "red";        // field
-        public int maxSpeed = 200;          // field
-        public void fullThrottle()   // method
-        {
-            Console.WriteLine("The car is going as fast as it can!");
-
-        }*/
     }
 }
--- Car/Car/Program.cs
+++ Car/Car/Program.cs
@@ -1,31 +1,72 @@
 ﻿using System;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.ComTypes;
 
 namespace Car
 {
 
-    class Jármű
+    class Person
     {
-        public string márka = "Ford";
-        public void duda()
+        private string név; // field
+        public string Név   // property - Tulajdonság
         {
-            Console.WriteLine("Tu tu");
+            get; set;
+            /*get { return név; }
+            set { név = value; }*/
         }
     }
-    class Autó : Jármű
-    {
-        public string modelName = "Mustang";
 
-    }
     class Program
     {
-        
+
+        public string name; //Class Program konstruktora fogja használni, változó
+        public Program() //Class Program konstruktora.
+        {
+            name = "Béla";
+        }
+        static void myMethod1(string szöveg) /*static = a metódus az adott osztályba tartozik,
+                                * nem pedig egy objektuma az osztálynak: Objektumpéldány nélkül is hívható
+                                * 
+                                * public – nyilvános, mindenki számára hozzáférhető
+                                * private – privát
+                                * protected – származtatott osztály hozzáférhet
+                                * static – statikus, objektumpéldány nélkül is hívható
+                                * 
+                                * <Access Specifier> <Return Type> <Method Name>(Parameter List) {
+                                  Method Body}*/
+        {
+            Console.WriteLine(szöveg + "piros");
+        }
+
+        static int MyMethod2(int x) //int, float, double lehet.
+        {
+            return 5 + x;
+        }
         static void Main(string[] args)
         {
-            Autó myObj = new Autó();
-            myObj.duda();
-            Console.WriteLine(myObj.márka + " " + myObj.modelName);
-            
+            Car_autó myObj = new Car_autó();
+            Console.WriteLine(myObj.color);
+            myMethod1("Vagyis: ");
+            //Program ojjektumpeldany = new Program();
+            //ojjektumpeldany.myMethod1();
+            Console.WriteLine(MyMethod2(3));
+
+            //KONSTRUKTORHOZ:
+            Program myObj2 = new Program();
+            Console.WriteLine(myObj2.name);
+
+            //GET, SET
+            Person myObj3 = new Person();
+            myObj3.Név = "Tomi";
+            Console.WriteLine(myObj3.Név);
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
