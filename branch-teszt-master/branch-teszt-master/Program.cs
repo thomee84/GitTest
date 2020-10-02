49cd3bada6ed938274dfcc526f365db985c9b51f
Tamas Csendes <tacsendes@gmail.com>
2020-10-02 19:37:39 +0200

    Branch-teszt-master

---
+++ branch-teszt-master/branch-teszt-master/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace branch_teszt_master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
